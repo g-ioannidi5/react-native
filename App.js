36a4403599750431d219d4380fb775f4c593e15c
Giorgos Ioannidis <giorgos@dataffirm.com>
2018-02-26 00:27:16 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+
+class Blah extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isShowingText: true};
+
+        setInterval(() => {
+            this.setState(previousState => {
+                return {isShowingText: !previousState.isShowingText};
+            })
+        }, 100000);
+    }
+
+    render() {
+        let display = this.state.isShowingText ? this.props.text : ' ';
+        return (
+            <Text> {display}</Text>
+        )
+    }
+}
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <Text>Changes you make will automatically reload.</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+                <Blah text="something"/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bigblue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    red: {
+        color: 'red',
+    },
 });
+
